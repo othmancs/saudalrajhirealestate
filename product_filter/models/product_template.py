8b7d064f3e493e0da4652bf6201c3771b0a5c6fc
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-09 15:39:49 +0300

    Update product_template.py

--- product_filter/models/product_template.py
+++ product_filter/models/product_template.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    unit_state = fields.Char(string='Unit State', related='product_variant_ids.unit_state')
+    unit_state = fields.Char(string='Unit State', help="State of the product unit")
