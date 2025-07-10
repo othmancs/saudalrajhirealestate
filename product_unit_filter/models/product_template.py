b333aa6b081c8c2189205ad3a7d49a1fd7f4f19c
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-10 12:49:04 +0300

    Update product_template.py

--- product_unit_filter/models/product_template.py
+++ product_unit_filter/models/product_template.py
@@ -3,13 +3,9 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    unit_information_id = fields.Many2one('unit.information', string='Unit Information')
-
     sale_visible = fields.Boolean(string='Visible in Sales', compute='_compute_sale_visible')
 
-    @api.depends('unit_information_id.unit_state')
+    @api.depends('unit_state')  # الحقل موجود داخل نفس الموديل
     def _compute_sale_visible(self):
         for rec in self:
-            # يعرض المنتج فقط إذا كانت قيمة unit_state هي "شاغرة"
-            unit_state = rec.unit_information_id.unit_state if rec.unit_information_id else ''
-            rec.sale_visible = (unit_state == 'شاغرة')
+            rec.sale_visible = (rec.unit_state == 'شاغرة')
