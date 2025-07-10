12d4cf530b1d87227165e5505bc10131e7cb283b
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-10 09:23:43 +0300

    Update product_template.py

--- product_filter/models/product_template.py
+++ product_filter/models/product_template.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    unit_state = fields.Boolean(related='product_id.unit_state', store=True, readonly=True)
+    unit_state = fields.Boolean(related='product_tmpl_id.unit_state', store=True, readonly=True)
