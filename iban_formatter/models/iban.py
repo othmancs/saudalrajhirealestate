81211abd36656f227b886a84454fd9be7e555d40
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-13 14:03:39 +0300

    Update iban.py

--- iban_formatter/models/iban.py
+++ iban_formatter/models/iban.py
@@ -34,31 +34,24 @@ from odoo.exceptions import UserError, ValidationError
 import logging
 
 
-class res_partner_bank(models.Model):
+class ResPartnerBank(models.Model):
     _inherit = "res.partner.bank"
 
     def _format_iban(self, acc_number):
         '''
-        This function removes all characters from given 'string' that isn't a alpha numeric and converts it to upper case, checks checksums and groups by 4
+        This function removes all characters from given 'string' that isn't alpha numeric,
+        converts it to upper case, checks checksums and groups by 4
         '''
         res = ''
         if acc_number:
-            # _logger = logging.getLogger(__name__)
-            # _logger.dbug('FGF acc_number %s' % (acc_number))
             try:
                 a = iban.validate(acc_number)
             except:
                 raise ValidationError(_('%s is not a valid IBAN.') % (acc_number))
-            res = iban.format(a) 
+            res = iban.format(a)
         return res
 
     @api.onchange('acc_number')
     def onchange_acc_id(self):
-        result = {}
         if self.acc_number:
-            result['acc_number'] = self._format_iban(self.acc_number)
-
-        return {'value': result}
-     
- 
-res_partner_bank() 
+            self.acc_number = self._format_iban(self.acc_number)
