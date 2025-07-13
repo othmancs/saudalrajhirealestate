c73755634b7a354923433431599ce99eb7b5bc1e
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-13 13:42:18 +0300

    Update main.py

--- account_parent/controllers/main.py
+++ account_parent/controllers/main.py
@@ -7,10 +7,10 @@
 #    For more details, check COPYRIGHT and LICENSE files
 #
 ##############################################################################
+
 from odoo import http, _
 from odoo.http import request, serialize_exception
 from odoo.tools import html_escape
-from odoo.addons.web.controllers.export import SpreadsheetExport
 from odoo.exceptions import UserError
 
 import json
@@ -55,18 +55,20 @@ class CoAReportController(http.Controller):
         report_obj = request.env['account.open.chart'].browse(report_id)
         user_context = report_obj._build_contexts()
 
-        lines = request.env['account.open.chart'].with_context(print_mode=True,
-                                                               output_format='xls'
-                                                               ).get_pdf_lines(report_id)
+        lines = request.env['account.open.chart'].with_context(
+            print_mode=True,
+            output_format='xls'
+        ).get_pdf_lines(report_id)
+
         user_context.update(report_obj.generate_report_context(user_context))
         show_initial_balance = user_context.get('show_initial_balance')
         row_data = report_obj.get_xls_title(user_context)
 
         if show_initial_balance:
-            row_data.append(
-                ['Code', 'Name', 'Type', 'Initial Balance', 'Debit', 'Credit', 'Ending Balance', 'Unfoldable'])
+            row_data.append(['Code', 'Name', 'Type', 'Initial Balance', 'Debit', 'Credit', 'Ending Balance', 'Unfoldable'])
         else:
             row_data.append(['Code', 'Name', 'Type', 'Debit', 'Credit', 'Balance', 'Unfoldable'])
+
         for line in lines:
             level = line.get('level')
             unfoldable = line.get('unfoldable')
@@ -79,11 +81,10 @@ class CoAReportController(http.Controller):
             balance = line.get('balance')
             if show_initial_balance:
                 balance = line.get('ending_balance')
-                row_data.append([code, name, ac_type, initial_balance, debit, credit,
-                                 balance, unfoldable])
+                row_data.append([code, name, ac_type, initial_balance, debit, credit, balance, unfoldable])
             else:
-                row_data.append([code, name, ac_type, debit, credit,
-                                 balance, unfoldable])
+                row_data.append([code, name, ac_type, debit, credit, balance, unfoldable])
+
         columns_headers = ['', '', 'Chart Of Accounts', '', '']
         rows = row_data
         return request.make_response(
@@ -101,7 +102,7 @@ class CoAReportController(http.Controller):
         output = io.BytesIO()
         workbook = xlsxwriter.Workbook(output, {'in_memory': True})
         worksheet = workbook.add_worksheet('Sheet 1')
-        
+
         # Header Style
         header_style = workbook.add_format({
             'bold': True,
@@ -110,13 +111,13 @@ class CoAReportController(http.Controller):
             'border': 1,
             'bg_color': '#FFFF00'
         })
-        
+
         # Data Style
         data_style = workbook.add_format({
             'text_wrap': True,
             'border': 1
         })
-        
+
         bold_style = workbook.add_format({
             'bold': True,
             'border': 1
@@ -125,20 +126,20 @@ class CoAReportController(http.Controller):
         # Write Headers
         for col, field in enumerate(fields):
             worksheet.write(0, col, field, header_style)
-            worksheet.set_column(col, col, 20)  # Adjust column width
+            worksheet.set_column(col, col, 20)
 
         # Write Data Rows
         for row_idx, row in enumerate(rows):
             unfoldable = row[-1]
             row.pop(-1)
-            
+
             for col_idx, cell_value in enumerate(row):
                 style = bold_style if unfoldable or row_idx + 1 in [2, 5] else data_style
-                
+
                 if isinstance(cell_value, str):
                     cell_value = re.sub("\r", " ", cell_value)
-                    cell_value = cell_value[:32767]  # Excel cell limit
-                
+                    cell_value = cell_value[:32767]
+
                 worksheet.write(row_idx + 1, col_idx, cell_value, style)
 
         workbook.close()
