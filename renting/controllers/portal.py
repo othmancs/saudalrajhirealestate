f56d0536d022407b764493b18d903afaed139211
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-14 07:58:54 +0300

    Update portal.py

--- renting/controllers/portal.py
+++ renting/controllers/portal.py
@@ -3,11 +3,8 @@ import binascii
 from odoo import fields, http, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
-# from odoo.addons.payment.controllers.portal import PaymentProcessing
-from odoo.addons.portal.controllers.mail import _message_post_helper
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
 from odoo.osv import expression
-from odoo import http
 from odoo.addons.sale.controllers.portal import CustomerPortal  # Import the class
 
 
@@ -40,13 +37,13 @@ class CustomCustomerPortal(CustomerPortal):  # Inherit in your custom class
             order_sudo.action_confirm()
             order_sudo._send_order_confirmation_mail()
 
-        pdf = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([order_sudo.id])[
-            0]
+        pdf = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([order_sudo.id])[0]
 
-        _message_post_helper(
-            'sale.order', order_sudo.id, _('Order signed by %s') % (name,),
+        # ✅ Use message_post instead of _message_post_helper
+        order_sudo.message_post(
+            body=_('Order signed by %s') % (name,),
             attachments=[('%s.pdf' % order_sudo.name, pdf)],
-            **({'token': access_token} if access_token else {}))
+        )
 
         query_string = '&message=sign_ok'
         if order_sudo.has_to_be_paid(True):
