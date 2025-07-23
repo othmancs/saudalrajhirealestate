f4b1d17ddd361e6c6cf5c168ec8af7b65a50fdcc
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-23 08:33:16 +0300

    Update __manifest__.py

--- ent_ohrms_loan/__manifest__.py
+++ ent_ohrms_loan/__manifest__.py
@@ -19,7 +19,7 @@
 #    DEALINGS IN THE SOFTWARE.
 #
 ########################################################################################
-{
+{ 
     'name': 'Enterprise Open HRMS Loan Management',
     'version': '15.0.1.0.0',
     'summary': 'Manage Loan Requests',
